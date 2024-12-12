842b4a1b4848f44052394e6fd4304aab3df10bee
Fadil Hisyam <ichorvfx@gmail.com>
2024-12-12 23:20:09 +0700

    chore: implement responsive layout

--- src/resumid_frontend/src/parts/HomePage/Achievement.tsx
+++ src/resumid_frontend/src/parts/HomePage/Achievement.tsx
@@ -1,7 +1,7 @@
 function Achievement() {
   return (
     <section className="responsive-container bg-[#f4f4f4] py-8 border-b border-neutral-200">
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <ul className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <li className="flex flex-col gap-1 justify-center items-center">
           <h4 className="text-balance font-inter text-heading text-center">
             🚀 <b className="font-semibold">Resumes analyzed</b> so far!
@@ -12,7 +12,7 @@ function Achievement() {
         </li>
         <li className="flex flex-col gap-1 justify-center items-center">
           <h4 className="text-balance font-inter text-heading text-center">
-            🎯 <b className="font-semibold">Increased Interview Rate</b> among users
+            🎯 <b className="font-semibold">Increased interview rate</b> among users
           </h4>
           <p className="font-outfit text-primary-500 text-3xl font-semibold">
             72.33%
