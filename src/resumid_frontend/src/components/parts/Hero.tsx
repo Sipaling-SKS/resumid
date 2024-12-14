598efd83759296dfd1e21a79f27298e09959f897
Fadil Hisyam <ichorvfx@gmail.com>
2024-12-14 14:34:06 +0700

    chore: modify spacing

--- src/resumid_frontend/src/components/parts/Hero.tsx
+++ src/resumid_frontend/src/components/parts/Hero.tsx
@@ -5,12 +5,12 @@ import HeroIllustration from "@/assets/hero-illustration.svg";
 
 function Hero() {
   return (
-    <header className="relative responsive-container overflow-clip flex flex-col md:flex-row justify-center md:justify-between items-center md:py-16 border-b border-neutral-200 min-h-screen md:min-h-[70vh]">
-      <div className="w-full md:w-3/5 pb-24 md:pb-0">
+    <header className="relative responsive-container overflow-clip flex flex-col md:flex-row justify-center md:justify-between items-center md:py-16 border-b border-neutral-200 min-h-[calc(100vh-73px)] md:min-h-[70vh]">
+      <div className="w-full md:w-3/5 pb-[12vh] md:pb-0">
         <h1 className="font-outfit font-semibold text-4xl md:text-[40px] text-balance leading-tight text-heading">
           Unleash Your Resume’s Potential with <Highlight>AI-Powered</Highlight> Analysis
         </h1>
-        <p className="font-inter text-[18px] text-paragraph mt-4">Decentralized, Insightful, and Ready to Elevate Your Career.</p>
+        <p className="w-4/5 font-inter text-[18px] text-paragraph mt-4">Decentralized, Insightful, and Ready to Elevate Your Career.</p>
         <Button className="mt-10 pr-5 w-full sm:w-fit" variant="gradient" size="lg">
           Analyze Your Resume
         <ArrowRightIcon />
