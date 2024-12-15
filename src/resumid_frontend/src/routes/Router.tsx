2b0b9d6ea9e8da077a06e836c33e9c2e0a2b5a0a
Fadil Hisyam <ichorvfx@gmail.com>
2024-12-15 16:02:46 +0700

    feat: initialize structure and router for result and analyzer page

--- src/resumid_frontend/src/routes/Router.tsx
+++ src/resumid_frontend/src/routes/Router.tsx
@@ -1,12 +1,17 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 
 // Page Imports
 import Home from "@/pages/Home";
+import Analyzer from "@/pages/Analyzer";
+import Result from "@/pages/Analyzer/Result";
 
 function Router() {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path="/resume-analyzer" element={<Analyzer />} />
+      <Route path="/result/:id" element={<Result />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
