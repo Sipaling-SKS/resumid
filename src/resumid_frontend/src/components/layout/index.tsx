134d353127f4823af0ca9ff68956ae5ccdb6884e
Fadil Hisyam <ichorvfx@gmail.com>
2024-12-15 23:01:27 +0700

    fix: restructure layout to optimize rendering

--- src/resumid_frontend/src/components/layout/index.tsx
+++ src/resumid_frontend/src/components/layout/index.tsx
@@ -1,29 +1,19 @@
 import { ReactNode } from "react"
 import { cn } from "@/lib/utils";
-import { Helmet } from "react-helmet"
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import { Outlet } from "react-router";
 
 interface LayoutProps {
-  children: ReactNode;
   className?: string;
-  title?: string;
-  description?: string;
 }
 
-function Layout({ children, className, title, description }: LayoutProps) {
-  const headTitle: string = title ? `${title} - Resumid` : "Resumid - Unleash Your Resume’s Potential with AI-Powered Analysis"
-
+function Layout({ className }: LayoutProps) {
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{headTitle}</title>
-        <meta name="description" content={description} />
-      </Helmet>
       <Navbar />
       <main className={cn("min-h-screen", className)}>
-        {children}
+        <Outlet />
       </main>
       <Footer />
     </>
