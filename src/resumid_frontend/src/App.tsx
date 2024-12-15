5b66c684754cf55f08f3b80067077ed6b2195ac7
Fadil Hisyam <ichorvfx@gmail.com>
2024-12-15 22:59:23 +0700

    feat: add toast component

--- src/resumid_frontend/src/App.tsx
+++ src/resumid_frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import Router from "@/routes/Router";
 import { DefaultScrollToTop } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   // Initial Load Logic Here
@@ -8,6 +9,7 @@ function App() {
     <>
       <DefaultScrollToTop />
       <Router />
+      <Toaster />
     </>
   );
 }
