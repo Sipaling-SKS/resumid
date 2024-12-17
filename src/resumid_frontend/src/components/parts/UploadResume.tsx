78479feb3a4b2f82d33582c33f532176ea6f77a7
Fadil Hisyam <ichorvfx@gmail.com>
2024-12-17 14:17:29 +0700

    chore: implement upload resume

--- src/resumid_frontend/src/components/parts/UploadResume.tsx
+++ src/resumid_frontend/src/components/parts/UploadResume.tsx
@@ -1,12 +1,21 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { FileInput } from "@/components/ui/file-input";
 import { Button } from "@/components/ui/button";
+import { toast } from "@/hooks/use-toast";
+import { Loader2 } from "lucide-react";
 
 interface UploadResumeProps {
-  onSubmit: (file: File) => void
+  onSubmit: (file: File) => void;
+  isLoading?: boolean;
+  file: File | null;
+  setFile: (file: File | null) => void;
 }
 
-function UploadResume({ onSubmit }: UploadResumeProps) {
+function UploadResume({ onSubmit, isLoading, file, setFile }: UploadResumeProps) {
+  const handleOnFileSelected = (value: File) => {
+    setFile(value)
+  }
+
   return (
     <Card className="space-y-5 md:space-y-6 p-6 md:p-8 max-w-md">
       <CardHeader>
@@ -20,15 +29,24 @@ function UploadResume({ onSubmit }: UploadResumeProps) {
       <hr className="h-[1px] w-full bg-neutral-200" />
       <CardContent>
         <div className="space-y-4">
-          <FileInput onFileSelected={onSubmit} />
+          <FileInput onFileSelected={handleOnFileSelected} file={file} setFile={setFile} />
           <p className="w-full text-center font-inter text-paragraph">Readable Files: PDF Only</p>
         </div>
       </CardContent>
-      <div className="">
-        <Button className="w-full">
-          Next
-        </Button>
-      </div>
+      <Button
+        onClick={() => {
+          if (file) {
+            onSubmit(file);
+          } else {
+            toast({ description: "Please select a file to proceed.", variant: "destructive" });
+          }
+        }}
+        disabled={!file || isLoading}
+        className="w-full"
+      >
+        {isLoading && <Loader2 className="animate-spin" />}
+        Next
+      </Button>
     </Card>
   )
 }
--- src/resumid_frontend/src/pages/Analyzer/index.tsx
+++ src/resumid_frontend/src/pages/Analyzer/index.tsx
@@ -1,22 +1,84 @@
+import PreviewResume, { PreviewFormValues } from "@/components/parts/PreviewResume";
 import UploadResume from "@/components/parts/UploadResume";
-import { useState } from "react";
+import { toast } from "@/hooks/use-toast";
+import { cleanExtractedText, extractPDFContent } from "@/lib/pdf2text";
+import { FormEvent, useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
+import { SubmitHandler } from "react-hook-form";
+
+export type Resume = {
+  fullText: string
+  filename: string
+  jobTitle?: string
+  jobDescription?: string
+}
 
 function Analyzer() {
+  const [isLoading, setLoading] = useState<boolean>(false)
   const [step, setStep] = useState<number>(0)
 
-  const [resume, setResume] = useState<object | null>(null)
+  const [resume, setResume] = useState<Resume>({
+    fullText: "",
+    filename: "",
+  });
+  const [file, setFile] = useState<File | null>(null);
 
-  const processFile = (file: File) => {
+  const processFile = async (file: File) => {
+    if (resume.fullText !== "" && resume.filename === file?.name) {
+      setStep(1);
+      return;
+    }
 
+    setLoading(true)
+
+    try {
+      const text = await extractPDFContent(file);
+      const cleanedText = cleanExtractedText(text);
+
+      await new Promise((resolve) => setTimeout(resolve, 500));
+
+      setResume({
+        fullText: cleanedText,
+        filename: file.name || "default_file.pdf",
+      });
+      setStep(1);
+    } catch (error) {
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+        variant: "destructive",
+      })
+    }
+
+    setLoading(false)
+  }
+
+  const analyzeResume: SubmitHandler<PreviewFormValues> = async (data) => {
+    console.log({...resume, ...data})
   }
 
   const MultiStepPage = () => {
     switch (step) {
       case 0:
-        return <UploadResume onSubmit={processFile} />;
+        return (
+          <UploadResume
+            isLoading={isLoading}
+            onSubmit={processFile}
+            file={file}
+            setFile={setFile}
+          />
+        );
+      case 1:
+        return (
+          <PreviewResume
+            onSubmit={analyzeResume}
+            resume={resume}
+            setResume={setResume}
+            setStep={setStep}
+          />
+        );
       default:
-        return <UploadResume onSubmit={processFile} />;
+        return <h1>Default</h1>;
     }
   }
 
@@ -26,8 +88,8 @@ function Analyzer() {
         <meta charSet="utf-8" />
         <title>Upload to start analyzing - Resumid</title>
       </Helmet>
-      <main className="bg-background-950 min-h-screen responsive-container py-6 md:py-16">
-        <div className="mx-auto">
+      <main className="bg-background-950 min-h-screen responsive-container py-6 md:py-8">
+        <div className="flex justify-center">
           <MultiStepPage />
         </div>
       </main>
