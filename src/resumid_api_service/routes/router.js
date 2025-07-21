e7106fd7826234eef7d8cd3b6b56367928e076d9
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2025-07-21 16:12:17 +0700

    Reapply "Merge pull request #1 from Sipaling-SKS/feat/gemini-integration"
    This reverts commit 27622e4e127f41f9695904278d263319a48f8855.

--- src/resumid_api_service/routes/router.js
+++ src/resumid_api_service/routes/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const GptController = require("../controllers/GptRequestController");
+const GeminiController = require("../controllers/GeminiController");
 
 const router = express.Router();
 
@@ -9,5 +10,11 @@ router.post('/gpt-service', GptController.CreateAnalyzeResume);
 
 // Mockup endpoint to return open ai gpt response mockup (static response)
 router.post('/gpt-mockup', GptController.CreateMockupAnalyzeResume);
+
+// Connected endpoint with open ai gemini service
+router.post('/gemini-service', GeminiController.CreateAnalyzeResume);
+
+// Mockup endpoint to return open ai gemini response mockup (static response)
+router.post('/gemini-mockup', GeminiController.CreateMockupAnalyzeResume);
 
 module.exports = router;
