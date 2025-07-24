2cd522fcf675a32eeeacd9a169bb8a2bd974bf1c
Rafli Rayhan <raplirayhann@gmail.com>
2025-07-25 00:45:26 +0700

    fix: section analysis none result ui adjustment

--- src/resumid_frontend/src/routes/Router.tsx
+++ src/resumid_frontend/src/routes/Router.tsx
@@ -15,10 +15,9 @@ function Router() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/history-detail/:id" element={<HistoryDetail />} />
-        {/* Additional routes nanti ini benerin lagi */}
         <Route element={<ProtectedRoute redirectTo="/" />}>
           <Route path="/resume-analyzer" element={<Analyzer />} />
+          <Route path="/history-detail/:id" element={<HistoryDetail />} />
           <Route path="/result" element={<HistoryList />} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
